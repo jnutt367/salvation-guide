6ea7e464a51079a9d350cd528a0d5eb5718a0a63
Jason Nutt <jnutt367@gmail.com>
2022-07-13 21:23:42 -0400

    commit for new second corinthians

--- pages/index.js
+++ pages/index.js
@@ -78,17 +78,15 @@ Stand Firm
             className={styles.card}
           >
             <h2>Chapter 3 &rarr;</h2>
-            <p>DThe Man of Lawlessness
-2 Concerning the coming of our Lord Jesus Christ and our being gathered to him, we ask you, brothers and sisters, 2 not to become easily unsettled or alarmed by the teaching allegedly from us—whether by a prophecy or by word of mouth or by letter—asserting that the day of the Lord has already come. 3 Don’t let anyone deceive you in any way, for that day will not come until the rebellion occurs and the man of lawlessness[a] is revealed, the man doomed to destruction. 4 He will oppose and will exalt himself over everything that is called God or is worshiped, so that he sets himself up in God’s temple, proclaiming himself to be God.
+            <p>Request for Prayer
+3 As for other matters, brothers and sisters, pray for us that the message of the Lord may spread rapidly and be honored, just as it was with you. 2 And pray that we may be delivered from wicked and evil people, for not everyone has faith. 3 But the Lord is faithful, and he will strengthen you and protect you from the evil one. 4 We have confidence in the Lord that you are doing and will continue to do the things we command. 5 May the Lord direct your hearts into God’s love and Christ’s perseverance.
 
-5 Don’t you remember that when I was with you I used to tell you these things? 6 And now you know what is holding him back, so that he may be revealed at the proper time. 7 For the secret power of lawlessness is already at work; but the one who now holds it back will continue to do so till he is taken out of the way. 8 And then the lawless one will be revealed, whom the Lord Jesus will overthrow with the breath of his mouth and destroy by the splendor of his coming. 9 The coming of the lawless one will be in accordance with how Satan works. He will use all sorts of displays of power through signs and wonders that serve the lie, 10 and all the ways that wickedness deceives those who are perishing. They perish because they refused to love the truth and so be saved. 11 For this reason God sends them a powerful delusion so that they will believe the lie 12 and so that all will be condemned who have not believed the truth but have delighted in wickedness.
+Warning Against Idleness
+6 In the name of the Lord Jesus Christ, we command you, brothers and sisters, to keep away from every believer who is idle and disruptive and does not live according to the teaching[a] you received from us. 7 For you yourselves know how you ought to follow our example. We were not idle when we were with you, 8 nor did we eat anyone’s food without paying for it. On the contrary, we worked night and day, laboring and toiling so that we would not be a burden to any of you. 9 We did this, not because we do not have the right to such help, but in order to offer ourselves as a model for you to imitate. 10 For even when we were with you, we gave you this rule: “The one who is unwilling to work shall not eat.”
 
-Stand Firm
-13 But we ought always to thank God for you, brothers and sisters loved by the Lord, because God chose you as firstfruits[b] to be saved through the sanctifying work of the Spirit and through belief in the truth. 14 He called you to this through our gospel, that you might share in the glory of our Lord Jesus Christ.
+11 We hear that some among you are idle and disruptive. They are not busy; they are busybodies. 12 Such people we command and urge in the Lord Jesus Christ to settle down and earn the food they eat. 13 And as for you, brothers and sisters, never tire of doing what is good.
 
-15 So then, brothers and sisters, stand firm and hold fast to the teachings[c] we passed on to you, whether by word of mouth or by letter.
-
-16 May our Lord Jesus Christ himself and God our Father, who loved us and by his grace gave us eternal encouragement and good hope, 17 encourage your hearts and strengthen you in every good deed and word.</p>
+14 Take special note of anyone who does not obey our instruction in this letter. Do not associate with them, in order that they may feel ashamed. 15 Yet do not regard them as an enemy, but warn them as you would a fellow believer.</p>
           </div>
 
           <div
