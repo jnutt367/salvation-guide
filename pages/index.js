792cf4e438a28c66b0976f9522cd66d4b8f6a808
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-05-30 21:35:19 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,10 @@ export default function Home() {
                                 width={2000}
                                 priority
                               />
-          </div>                    
-                               <div className={styles.nav}>           
+         
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/oNNZO9i1Gjc" title="YouTube video player" frameborder="3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>           
+<div className={styles.nav}>           
       <a href="https://first-thessalonians.vercel.app/">
              <Image 
                                 src="/chevron-left.svg"
