9b753a03384d0d13bb1ee19dcf4c9d3be9cb61d2
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-06-03 21:48:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
          <div  className={styles.image}>
           
        <Image 
-                                src="/receiving_christ.png"
+                                src="/wordle-christ_is_god.webp"
                                 alt="Jesus wordle"
                                 height={900}
                                 width={2000}
