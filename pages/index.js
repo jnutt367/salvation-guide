5b7b92207c5bbc919327dd78074acdeaaddc0957
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-13 11:39:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,13 +28,9 @@ export default function Home() {
         <h1 className={styles.title}>
        ETERNAL LIFE
         </h1>
-         <div  className={styles.iframe1}>
-          
-      <iframe width="100%" height="300" src="https://www.youtube.com/embed/uCOycIMyJZM?si=2-AAha4hMfOVQxIC&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-         
-
-          </div>    
-
+     <div className={styles.iframe1}>
+                                      <iframe width="100%" height="300" src="https://www.youtube.com/embed/_c3WIBen-aA?si=-468bJiF5TooM2p1&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                  </div> 
  <p className={styles.parables}>
                                  “Repent & be baptized, every one of you, in the name of Jesus Christ for forgiveness of your sins.
     </p>  
@@ -82,10 +78,16 @@ export default function Home() {
 
 
       </div>
-                                  <div className={styles.iframe}>
-                                      <iframe width="100%" height="315" src="https://www.youtube.com/embed/_c3WIBen-aA?si=-468bJiF5TooM2p1&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                                  </div> 
+                                  
 
+    <div  className={styles.iframe}>
+          
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/uCOycIMyJZM?si=2-AAha4hMfOVQxIC&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+         
+
+          </div>    
+
+                                  
 <div className={styles.grid}>
           
            <div className={styles.card}>
